8ade926f988a1826cabfc65803a65841f0304e2b
masangana <alexmasangana@gmail.com>
2022-06-07 16:30:27 +0300

    task3 solved

--- task3.js
+++ task3.js
@@ -1,14 +1,11 @@
 
 class calculator{
-    constructor(a, b){
-        this.a = a;
-        this.b = b;
-    }
-
-    add = () => (this.a + this.b);
-    subtract = () => (this.a - this.b);
-    divide = () => (this.a / this.b);
-    multiply = () => (this.a * this.b);
+    
+    add = (a,b) => (a + b);
+    subtract = (a,b) => (a - b);
+    divide = (a,b) => (a / b);
+    multiply = (a,b) => (a * b);
 }
+
 
 module.exports = calculator;
--- task3.test.js
+++ task3.test.js
@@ -2,14 +2,92 @@ const calculator = require('./task3');
 
 describe('Add function', function() {
 
-    test('8+2 = 10', () => {
-        calculate(8,2) = new calculator
-        expect(calculate.add()).toBe(10);
+    test('7 + 2 = 9', () => {
+        calculate = new calculator
+        expect(calculate.add(7,2)).toBe(9);
     } )
 
     test('7 + (-2)', () => {
-        calculate(7,-2) = new calculator
-        expect(calculate.add()).toBe(5);
+        calculate = new calculator
+        expect(calculate.add(7,-2)).toBe(5);
     } )
 
+    test('(-7) + (-2)', () => {
+        calculate = new calculator
+        expect(calculate.add(-7,-2)).toBe(-9);
+    } )
+
+    test('(-7) + (2)', () => {
+        calculate = new calculator
+        expect(calculate.add(-7,2)).toBe(-5);
+    } )
+})
+
+describe('subtract function', function() {
+
+    test('7 - 2 = 9', () => {
+        calculate = new calculator
+        expect(calculate.subtract(7,2)).toBe(5);
+    } )
+
+    test('7 - (-2)', () => {
+        calculate = new calculator
+        expect(calculate.subtract(7,-2)).toBe(9);
+    } )
+
+    test('(-7) - (-2)', () => {
+        calculate = new calculator
+        expect(calculate.subtract(-7,-2)).toBe(-5);
+    } )
+
+    test('(-7) - (2)', () => {
+        calculate = new calculator
+        expect(calculate.subtract(-7,2)).toBe(-9);
+    } )
+})
+
+describe('divide function', function() {
+
+    test('6 / 2 = 9', () => {
+        calculate = new calculator
+        expect(calculate.divide(6,2)).toBe(3);
+    } )
+
+    test('6 / (-2)', () => {
+        calculate = new calculator
+        expect(calculate.divide(6,-2)).toBe(-3);
+    } )
+
+    test('(-6) / (-2)', () => {
+        calculate = new calculator
+        expect(calculate.divide(-6,-2)).toBe(3);
+    } )
+
+    test('(-6) / (2)', () => {
+        calculate = new calculator
+        expect(calculate.divide(-6,2)).toBe(-3);
+    } )
+})
+
+describe('subtract function', function() {
+
+    test('7 * 2 = 9', () => {
+        calculate = new calculator
+        expect(calculate.multiply(7,2)).toBe(14);
+    } )
+
+    test('7 * (-2)', () => {
+        calculate = new calculator
+        expect(calculate.multiply(7,-2)).toBe(-14);
+    } )
+
+    test('(-7) * (-2)', () => {
+        calculate = new calculator
+        expect(calculate.multiply(-7,-2)).toBe(14);
+    } )
+
+    test('(-7) * (2)', () => {
+        calculate = new calculator
+        expect(calculate.multiply(-7,2)).toBe(-14);
+    } )
 })
